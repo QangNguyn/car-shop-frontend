22d074cf32a72d273ba14c3ff008d7936d999bda
unknown <minhquanga32001@gmail.com>
2023-02-10 21:49:07 +0700

    create menu mobile

--- script/script.js
+++ script/script.js
@@ -23,3 +23,26 @@ btn.on("click", function (e) {
   e.preventDefault();
   $("html, body").animate({ scrollTop: 0 }, "300");
 });
+
+const menuMobileIcon = document.querySelector(".menu-mobile .menu-icon");
+const menuMobile = document.querySelector(".menu-mobile .menu-mobile__list");
+const menuMobileModalLayout = document.querySelector(
+  ".main-header .menu-mobile-modal"
+);
+const menuCloseBtn = document.querySelector(
+  ".menu-mobile__list .close-menu-mobile"
+);
+menuMobileIcon.addEventListener("click", () => {
+  menuMobile.classList.add("active");
+  menuMobileModalLayout.style.display = "block";
+});
+
+menuCloseBtn.addEventListener("click", () => {
+  menuMobile.classList.remove("active");
+  menuMobileModalLayout.style.display = "none";
+});
+
+menuMobileModalLayout.addEventListener("click", () => {
+  menuMobile.classList.remove("active");
+  menuMobileModalLayout.style.display = "none";
+});
