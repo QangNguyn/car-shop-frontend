65fb30360c176a9c247c4b2b1c3e3810cdaa74cd
MinhQuang <minhquanga32001@gmail.com>
2023-02-24 16:34:45 +0700

    add spinner

--- script/script.js
+++ script/script.js
@@ -56,37 +56,6 @@ lightbox.option({
   showImageNumberLabel: false,
 });
 
-let rangeInput = document.querySelectorAll(
-  ".filter-form__form--range-price input"
-);
-let rangeText = document.querySelectorAll(".range-price__text div");
-let progress = document.querySelector(".range-price__progress");
-
-let priceGap = 300000000;
-
-rangeInput.forEach((input) => {
-  let priceMax = rangeInput[0].max;
-  input.addEventListener("input", (event) => {
-    let minVal = parseInt(rangeInput[0].value);
-    let maxVal = parseInt(rangeInput[1].value);
-
-    if (maxVal - minVal < priceGap) {
-      if (event.target.className === "min-range") {
-        minVal = rangeInput[0].value = maxVal - priceGap;
-      } else {
-        maxVal = rangeInput[1].value = minVal + priceGap;
-      }
-    }
-
-    let positionMin = (minVal / priceMax) * 100;
-    let positionMax = 100 - (maxVal / priceMax) * 100;
-    progress.style.left = positionMin + "%";
-    progress.style.right = positionMax + "%";
-    rangeText[0].innerText = minVal.toLocaleString();
-    rangeText[1].innerText = maxVal.toLocaleString();
-  });
-});
-
 document.querySelectorAll(".filter-form__form--group").forEach((select) => {
   setupSelector(select);
 });
@@ -278,3 +247,9 @@ if (document.querySelector(".share-social-btn")) {
     return false;
   };
 }
+
+window.addEventListener("load", () => {
+  document.querySelector(".spinner").style.display = "none";
+  document.querySelector(".spinner").style.opacity = "0";
+  document.querySelector(".main").style.opacity = "1";
+});
